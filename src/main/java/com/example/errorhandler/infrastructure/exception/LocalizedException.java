989f2ba716b66c0de278bd4d150495ff65593bdd
Cem Aktas <cemaktas@ymail.com>
2024-11-22 11:18:33 +0300

    added class and method level documentation to LocalizedException.java

--- src/main/java/com/example/errorhandler/infrastructure/exception/LocalizedException.java
+++ src/main/java/com/example/errorhandler/infrastructure/exception/LocalizedException.java
@@ -13,6 +13,15 @@ import reactor.core.publisher.Mono;
 import java.util.Locale;
 import java.util.Map;
 
+/**
+ * A custom runtime exception that supports internationalization (i18n) by providing
+ * localized error messages and titles based on the user's locale. This exception is
+ * designed for use in reactive Spring WebFlux applications and leverages Reactor's
+ * context to propagate locale information throughout the reactive chain.
+ * When thrown, this exception can generate localized error messages using a
+ * {@link MessageSource}, and it supports fallback mechanisms in case a localized
+ * message is not found for the specified locale.
+ */
 @Slf4j
 public class LocalizedException extends RuntimeException {
 
@@ -29,6 +38,17 @@ public class LocalizedException extends RuntimeException {
         this.args = args;
     }
 
+    /**
+     * Retrieves a {@link Mono} that emits a map containing the localized error message and title.
+     * The method uses the Reactor context to obtain the user's locale, which is expected to be
+     * stored under the key defined in {@link ContextConstants#ACCEPT_LANGUAGE_CONTEXT_KEY}.
+     * If the locale is not found in the context, it defaults to {@link Locale#ENGLISH}.
+     * The method attempts to retrieve the localized messages from the provided {@link MessageSource}.
+     * If the messages are not found for the user's locale, it falls back to English. If they are
+     * still not found, default messages are used.
+     * @param messageSource The {@link MessageSource} used to retrieve localized messages.
+     * @return A {@link Mono} emitting a {@link Map} containing the localized "message" and "title".
+     */
     public Mono<Map<String, String>> getLocalizedMessage(MessageSource messageSource) {
         return Mono.deferContextual(contextView -> {
             Locale locale = contextView.getOrDefault(ContextConstants.ACCEPT_LANGUAGE_CONTEXT_KEY, Locale.ENGLISH);
